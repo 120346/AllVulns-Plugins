f44625ff6d7900eb86a4f580efe92deaa8f572a0
Richard George <rick.george@outlook.com>
2021-09-29 15:39:24 -0400

    adding icon

--- AllVulnsWebScanner/AllVulnsWebScanner.py
+++ AllVulnsWebScanner/AllVulnsWebScanner.py
@@ -8,29 +8,40 @@ all_Vulns_Web_Scanner=None
 class Scanner(BaseWebScanner):
     def scan(self, appName, scanDate, pentest, nmapInfo=None, hostString=None ):
         for ip in nmapInfo:
-            for port in nmapInfo[ip]:
-                url = nmapInfo[ip][port]['url']
+            for entry in nmapInfo[ip]:
+                if entry == 'reverseDNS':
+                    url = nmapInfo[ip]['reverseDNS']
+                else:
+                    port = entry
+                    url = nmapInfo[ip][port]['url']
                 if url != '':
-                    target = self.addTargetToPentest(str(uuid.uuid1()), pentest, url, "Initializing")
-                    try:
-                        vulns={}
-                        allVulns_Web_Scanner= allVulnsWebScanner.Scanner()
-                        vulns = allVulns_Web_Scanner.scan(url)
-                        target.status="completed"
-                        for affects_url in vulns:
-                            for _type in vulns[affects_url]:
-                                payload = vulns[affects_url][_type]['Payload']
-                                element = vulns[affects_url][_type]['Element']
-                                severity = vulns[affects_url][_type]['Severity']
-                                recomendation = vulns[affects_url][_type]['Recommendation']
-                                reference = vulns[affects_url][_type]['References']
-                                references = [{'href': vulns[affects_url][_type]['References'], 'rel': reference}]
-                                request = payload + " " + element
-                                self.storeVulnerability(target, severity, _type, affects_url, recomendation, request, references)
-
-                    except Exception as e:
-                        log.error(e)
-                        target.status = "failed"
-                    target.save()
+                    if isinstance(url, list):
+                        for uri_entry in url:
+                            self.scan_target(pentest, uri_entry.domain)
+                    else:
+                        self.scan_target(pentest, url)
         log.info("gathering evidence")
         self.gatherEvidence()
+
+    def scan_target(self, pentest, url):
+        target = self.addTargetToPentest(str(uuid.uuid1()), pentest, url, "Initializing")
+        try:
+            vulns = {}
+            allVulns_Web_Scanner = allVulnsWebScanner.Scanner()
+            vulns = allVulns_Web_Scanner.scan(url)
+            target.status = "completed"
+            for affects_url in vulns:
+                for _type in vulns[affects_url]:
+                    payload = vulns[affects_url][_type]['Payload']
+                    element = vulns[affects_url][_type]['Element']
+                    severity = vulns[affects_url][_type]['Severity']
+                    recomendation = vulns[affects_url][_type]['Recommendation']
+                    reference = vulns[affects_url][_type]['References']
+                    references = [{'href': vulns[affects_url][_type]['References'], 'rel': reference}]
+                    request = payload + " " + element
+                    self.storeVulnerability(target, severity, _type, affects_url, recomendation, request, references)
+
+        except Exception as e:
+            log.error(e)
+            target.status = "failed"
+        target.save()
